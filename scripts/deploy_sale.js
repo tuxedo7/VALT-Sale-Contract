71c676a6ca43dfeee1509c94d5cc84a4c7a05040
tuxedo7 <tommy.dev0127@gmail.com>
2025-05-09 23:39:41 -0700

    update deploy contract factory name

--- scripts/deploy_sale.js
+++ scripts/deploy_sale.js
@@ -8,11 +8,11 @@ async function main() {
   const usdtAddress = "0xde74fcF3FafBb00Ba73395aD734f293edfD04544";
   const valtAddress = "0xa207C8686c3a4256e6176b25E0AA7dAc468009D1";
 
-  const ValutSwap = await ethers.getContractFactory("ValutSwap");
+  const ValutSwap = await ethers.getContractFactory("ValtSwap");
   const swap = await ValutSwap.deploy(usdtAddress, valtAddress);
 
   await swap.waitForDeployment();
-  console.log("ValutSwap deployed to:", await swap.getAddress());
+  console.log("ValtSwap deployed to:", await swap.getAddress());
 }
 
 main().catch((error) => {
